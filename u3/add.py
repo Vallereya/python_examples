2448f4179264499db0ee729377e4d81c4fe9b53f
Vallereya <v@vallereya.com>
2026-02-01 16:51:09 -0500

    update add
    added the print for the updated total in list, added the reversed sort print, keeping same structure.

--- u3/add.py
+++ u3/add.py
@@ -1,5 +1,7 @@
+# Importing the list from `list.py`.
 from list import items
 
+# adding 5 new camping items to the existing list.
 items.extend([
     "stove",
     "backpack",
@@ -8,3 +10,11 @@ items.extend([
     "bug spray",
 ])
 
+# Print the total number of items in the list after extending.
+print("\nUpdated Total Items:", len(items))
+
+# Sort the list of items reversed alphabetically.
+print("Updated Reversed Sorted Items:")
+
+for item in sorted(items, reverse=True):
+    print("-", item)
