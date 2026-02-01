ce46d99f9f7820f9057732524179416824cabe6e
Vallereya <v@vallereya.com>
2026-02-01 17:39:34 -0500

    update add
    added a testing print for a non-reversed sorted list.

--- u3/add.py
+++ u3/add.py
@@ -23,9 +23,9 @@ print("\nUpdated Total Items:", len(extended_items))
 # Sort the list of items reversed alphabetically.
 print("Updated Reversed Sorted Items:")
 
-# for extended_item in sorted(extended_items, reverse=True):
-#     print("-", extended_item)
+for extended_item in sorted(extended_items, reverse=True):
+    print("-", extended_item)
 
 # Non-Reverse sorted list, for testing purposes only.
-for extended_item in sorted(extended_items):
-    print("-", extended_item)
+# for extended_item in sorted(extended_items):
+#     print("-", extended_item)
