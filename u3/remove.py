7327970eb23a4d11b98404151adfbac3b7136d28
Vallereya <v@vallereya.com>
2026-02-01 21:35:35 -0500

    update remove
    added new var to hold the final list.

--- u3/remove.py
+++ u3/remove.py
@@ -7,3 +7,6 @@ from replace import replaced_items
 # Removing `binoculars` from the existing list, if it exists.
 if "binoculars" in replaced_items:
     replaced_items.remove("binoculars")
+
+# Creating a new variable to hold the final list.
+final_items = replaced_items
