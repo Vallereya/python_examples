1595012f8839d623f549b3b90546cc8519b745a2
Vallereya <v@vallereya.com>
2026-02-01 21:20:53 -0500

    update replace
    added 3 prints using slice notation, made sure its also using main so I don't get multiple prints from the imported files.

--- u3/replace.py
+++ u3/replace.py
@@ -16,6 +16,28 @@ extended_items.sort()
 # Find the index of `binoculars` for later use.
 new_index = extended_items.index("binoculars")
 
-# Print the total number of items in the list after replacement.
-print("Updated Total Items After Replacement:", len(extended_items))
+# Updated so that it can be run as a script, 
+# when executed directly.
+if __name__ == "__main__":
 
+    # Print the total number of items in the list after replacement.
+    print("Updated Total Items After Replacement:", len(extended_items))
+
+    # Using Slice Notation to separate the list,
+    # Print the updated list before replacement first.
+    print("Items Before Replacement:")
+
+    for item in extended_items[:new_index]:
+        print("-", item)
+
+    # Then printing the new_index item.
+    print("\nNew Item:")
+
+    for item in extended_items[new_index:new_index+1]:
+        print("-", item)
+
+    # And finally print the updated list after replacement.
+    print("\nItems After Replacement:")
+
+    for item in extended_items[new_index+1:]:
+        print("-", item)
