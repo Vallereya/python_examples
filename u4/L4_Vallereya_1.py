4609778e4d42a12b3045f4db359675853e64905b
Vallereya <v@vallereya.com>
2026-02-08 18:22:06 -0500

    update card dealer
    added random func to get the cards/combine, then print them and print num dealt.

--- u4/L4_Vallereya_1.py
+++ u4/L4_Vallereya_1.py
@@ -45,3 +45,21 @@ dealt_cards = int(
 # Build a list of the cards to be dealt, no duplicates.
 hand = []
 
+while len(hand) < dealt_cards:
+
+    # Get a random value and suit,
+    # then combine them to make a card.
+    value = random.choice(values)
+    suit = random.choice(suits)
+    card = value + suit
+
+    # If the card is not already in the hand, add it.
+    if card not in hand:
+        hand.append(card)
+    
+# Print the hand of cards.
+for card in hand:
+    print(card)
+
+# Print the number of cards that were dealt.
+print("Total cards dealt:", dealt_cards)
