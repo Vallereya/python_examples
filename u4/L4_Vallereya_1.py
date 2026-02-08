ddda3c97901a66d22b7e28747b151f6a94a66445
Vallereya <v@vallereya.com>
2026-02-08 17:54:43 -0500

    added example for u4

---
+++ u4/L4_Vallereya_1.py
@@ -0,0 +1,13 @@
+"""
+    Program Name:   Deal Cards
+    Author:         Vallereya
+    Date:           2025-02-08
+    Starter Code:   N/A
+    Purpose:        This program will simulate a simple
+                    dealing of cards. The user will specify
+                    how many they want, and then the program
+                    will randomly generate that many cards by
+                    combining a random rank and suit.
+"""
+
+import random
