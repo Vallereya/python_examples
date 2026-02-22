a472f0f7212d152e26219485771f517c608d03a0
Vallereya <v@vallereya.com>
2026-02-22 13:42:53 -0500

    u6 example
    added example for login system

---
+++ u6/L6_Vallereya_1.py
@@ -0,0 +1,18 @@
+"""
+    Program Name:   Login System
+    Author:         Vallereya
+    Date:           2025-02-22
+    Starter Code:   N/A
+    Purpose:        This program will simulate a simple
+                    login system using a dictionary to 
+                    store usernames and passwords.
+"""
+
+def main():
+
+    # Create a dictionary to store usernames and passwords.
+    users = {
+        "guest": "guest",
+    }
+
+
